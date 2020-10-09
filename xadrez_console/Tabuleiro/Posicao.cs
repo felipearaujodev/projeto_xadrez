a42aebff877c86a423143c4a961c7d6a6dbfb94c
Felipe Araujo <comunicar.felipearaujo@gmail.com>
2020-10-09 08:18:19 -0300

    Criando a classe Posicao

---
+++ xadrez_console/Tabuleiro/Posicao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    class Posicao
+    {
+        public int _linha { get; set; }
+        public int _coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            _linha = linha;
+            _coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return _linha + ", " + _coluna;
+        }
+    }
+}
